0f3ce5d36b6ee4a0c30675877e411b8175c398d9
aRod209 <afr209@gmail.com>
2019-06-24 10:02:49 -0700

    updated comments

--- Paddle.java
+++ Paddle.java
@@ -5,11 +5,15 @@
  * @since 2019-06-01
  *****************************************************************************/
 public class Paddle {
-    private double x;
-    private double y = 0.0; // Coordinates
+    private double x; // x-coordinate
+    private double y = 0.0; // y-coordinate
     private static double rx = 0.03, ry = 0.13; // Radii in the x and y directions;
     private static double v = 0.015;
 
+    /**
+     * Constructor.
+     * @param x
+     */
     Paddle(double x) {
         this.x = x;
     }
@@ -52,18 +56,34 @@ public class Paddle {
         if (y - ry > -Game.HALF_SCREEN_LENGTH) { y -= v; }
     }
 
+    /**
+     * Gives y-coordinate of top border of paddle.
+     * @return y-coordinate of top border.
+     */
     public double topBorder() {
         return y + ry;
     }
 
+    /**
+     * Gives y-coordinate of bottom border of paddle.
+     * @return y-coordinate of bottom border.
+     */
     public double bottomBorder() {
         return y - ry;
     }
 
+    /**
+     * Gives x-coordinate of right border of paddle.
+     * @return x-coordinate of right border.
+     */
     public double rightBorder() {
         return x + rx;
     }
 
+    /**
+     * Gives x-coordinate of left border of paddle.
+     * @return x-coordinate of left border.
+     */
     public double leftBorder() {
         return x - rx;
     }
